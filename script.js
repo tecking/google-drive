72e1e7f09c7d00e49d98ac2f92270b17acc651b7
tecking <tets.morikawa@gmail.com>
2020-05-02 15:10:59 +0900

    CSS, JS 分離

---
+++ script.js
@@ -0,0 +1,32 @@
+const date = new Date(), year = date.getFullYear();
+document.getElementById('copyright').innerHTML = '&#169;' + year + ' tecking.';
+
+function copy() {
+	let copyText = document.querySelector("#direct-url");
+	copyText.select();
+	document.execCommand("copy");
+}
+document.querySelector("#copy").addEventListener("click", copy);
+
+
+$(function() {
+	$('#submit').click(function() {
+		let url, id, img;
+		function escapeHTML(val) {
+			return $('<div>').text(val).html();
+		};
+
+		url = encodeURI( $('#shared-url').val() );
+		if ( url.indexOf( 'drive.google.com', 0 ) == -1 ) {
+			alert( 'Google DriveのURLではありません' );
+			return false;
+		}
+		console.log (url);
+
+		id  = url.match(/(^.+?file\/d\/)(.+?)(\/.+?$)/);
+		img = 'https://drive.google.com/uc?export=view&id=' + id[2];
+		$('#direct-url').val(img);
+		$('#preview-image img').remove();
+		$('#preview-image').append('<img style="width: 200px;" class="img-thumbnail" src="' + img + '" alt="">');
+	});
+});
